557b190b5dfb8c1e1fdba61ddab5a7c99d1ce2a2
JuliaHusar <juliahusar03@gmail.com>
2026-03-04 09:55:41 -0500

    Moved unit visualization to separate React Component

--- src/Pages/HistoricData.jsx
+++ src/Pages/HistoricData.jsx
@@ -1,14 +1,11 @@
-import * as d3 from 'd3';
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import Papa from "papaparse";
 import '../App.css';
-import {convertToDate, datesWithinRange, downloadSVG} from "../Components/Math/HelperFunctions.js";
 import HourOverTimeCo2 from "../Components/Graphs/HourOverTimeCo2.jsx";
+import UnitVis from "../Components/Graphs/UnitVis.jsx";
 
 const HistoricData = () => {
-    const ref = useRef();
     const [historicData, setHistoricData] = useState([]);
-    const [rawCSV, setRawCSV] = useState(null)
     const [selectedData, setSelectedData] = useState(null);
     const width = 3000;
     const height = 900;
@@ -31,8 +28,6 @@ const HistoricData = () => {
 
                  */
                 const text = await response.text();
-                setRawCSV(text)
-
                 Papa.parse(text, {
                     complete: (results) => {
                         results.data.pop();
@@ -215,203 +210,7 @@ const HistoricData = () => {
     }, [historicData, selectedData]);
 
      */
-    useEffect(() => {
-        if (historicData.length === 0) return;
 
-        //data cleaning
-        const dailyData = d3.group(historicData, d => {
-            const date = new Date(d.LocalTime);
-            return d3.timeDay(date).toISOString().split('T')[0];
-        });
-        const dailyArray = Array.from(dailyData, ([date, values]) => ({ date, values }));
-        //svg init
-        const svg = d3.select(ref.current)
-            .attr('width', width)
-            .attr('height', height);
-
-        //const tooltip = d3.select('#tooltip');
-        const dates = dailyArray.map(d => (d.date));
-
-        const x = d3.scaleBand()
-            .domain(dates.map(d => (d)))
-            .range([margin.left, width - margin.right]);
-        const y = d3.scaleLinear()
-            .domain([0, 24])
-            .range([margin.top, height - margin.bottom]);
-
-        //create cells for graph
-        const cellWidth = x.bandwidth();
-        const cells = svg.selectAll('g.cell')
-            .data(dailyArray)
-            .enter()
-            .append('g')
-            .attr('class', 'cell')
-            .attr('transform', d => `translate(${x(d.date)}, ${margin.top})`);
-        const smallMargin = { top: 10, right: 10, bottom: 20, left: 30 };
-        cells.each(function(d) {
-            const cell = d3.select(this);
-            //helper function to get maxCo2 at any point
-            const maxCo2 = () => {
-                return Math.max(
-                    d3.max(historicData, d => d.Co2_In),
-                    d3.max(historicData, d => d.Co2_Out)
-                )
-            }
-            //x-axis to display lux values on path
-            const luxAxis = d3.scaleLinear()
-                .domain([0, d3.max(historicData, d => d.front_lux_values)])
-                .range([smallMargin.left, cellWidth - smallMargin.right]);
-            //x-axis used to display count
-            const co2Axis = d3.scaleLinear()
-                .domain([0, maxCo2()])
-                .range([smallMargin.left, cellWidth - smallMargin.right]);
-            //matches up with outerY scale
-            const innerY = d3.scaleLinear()
-                .domain([0, 24])
-                .range([0, height - margin.bottom - margin.top]);
-            //lux values that are plotted on the line, rn it's only getting front lux values
-            // but we can change it to measure how much light is passing through bioreactor (the more carbon it sequesters, the less light goes through)
-            const line = d3.line()
-                .defined((d, i, data) => {
-                    if (i === 0) return true;
-                    const prev = new Date(data[i - 1].LocalTime);
-                    const curr = new Date(d.LocalTime);
-                    const diffHours = (curr - prev) / 1000 / 60 / 60;
-                    return diffHours < 1;
-                })
-                .x(d => luxAxis(d.front_lux_values))
-                .y(d => {
-                    const t = new Date(d.LocalTime);
-                    return innerY(t.getHours() + t.getMinutes() / 60);
-                })
-            /*
-            displays y-axis for every individual cell, removed it bcs of clutter but we can always add it back
-            cell.append('g')
-                .attr('transform', `translate(${smallMargin.left}, 0)`)
-                .call(d3.axisLeft(innerY)
-                    .ticks(24)
-                    .tickFormat(d => `${String(Math.floor(d)).padStart(2, '0')}:00`)
-                )
-
-             */
-            //bins for yscale based on hour of day
-            const binY = d3.scaleLinear()
-                .domain([0, 24])
-                .range([0, height - margin.top - margin.bottom]);
-            //histogram for the co2 values with circles
-            const histogram = d3.histogram()
-                .value(d => {
-                    const t = new Date(d.LocalTime);
-                    return t.getHours() + t.getMinutes() / 60;
-                })
-                .domain([0, 24])
-                .thresholds(d3.range(0, 24, 1));
-            //get bin intervals based on co2 values
-            //we can change this later to account for all co2 values if needed
-            const bins = histogram(d.values.filter(v => v.Co2_In != null));
-            //for every bin, create a dot count
-            bins.forEach(timeBin => {
-                if (timeBin.length === 0) return;
-
-                const meanCo2In = d3.mean(timeBin, v => v.Co2_In);
-                const meanCo2Out = d3.mean(timeBin, v => v.Co2_Out);
-                const dotCountIn = Math.round(meanCo2In / 100);
-                const dotCountOut = Math.round(meanCo2Out / 100);
-                const r = 5;
-                //maps out dots for ever bin within the cell on the y-axis
-                d3.range(dotCountIn).forEach(i => {
-                    cell.append("circle")
-                        .attr("cx", smallMargin.left + r + i * (r * 2 + 2))
-                        .attr("cy", binY((timeBin.x0 + timeBin.x1) / 2))
-                        .attr("r", r)
-                        .attr("fill", "gray") //input is gray
-                        .attr("opacity", 1);
-                });
-                d3.range(dotCountOut).forEach(i => {
-                    cell.append("circle")
-                        .attr("cx", smallMargin.left + r + i * (r * 2 + 2))
-                        .attr("cy", binY((timeBin.x0 + timeBin.x1) / 2))
-                        .attr("r", r)
-                        .attr("fill", "green") //output is green indicating what was reduced
-                        .attr("opacity", 1);
-                });
-            });
-
-            cell.append('g')
-                .attr('transform', `translate(0, ${height - margin.top - margin.bottom})`)
-                .call(d3.axisBottom(luxAxis).ticks(3).tickSizeInner(-6))
-                .selectAll('text')
-                .attr('text-anchor', 'end')
-                .attr('dx', '1.5em')
-                .attr('dy', '-1.5em')
-
-
-            const n = 5;
-            const dotsPerCircle = 100;
-            const sampledData = d.values.filter((_, i) => i % n === 0);
-
-            cell.selectAll("circle")
-                .data(bins)
-                .enter()
-
-            cell.append('g')
-                .selectAll("g.bin")
-                .data(bins)
-                .enter()
-                .append("g")
-                .attr("class", "bin")
-/* These are scatterplots of the points that form bands showing the input and output values
-            cell.append('g')
-                .selectAll("circle")
-                .data(d.values)
-                .enter()
-                .append("circle")
-                .attr("cx", v => co2Axis(v.Co2_In))
-                .attr("cy", v => {
-                    const t = new Date(v.LocalTime);
-                    return innerY(t.getHours() + t.getMinutes() / 60);
-                })
-                .attr("r", 2)
-                .attr("fill", "steelblue");
-
-            cell.append('g')
-                .selectAll("circle")
-                .data(d.values)
-                .enter()
-                .append("circle")
-                .attr("cx", v => co2Axis(v.Co2_Out))
-                .attr("cy", v => {
-                    const t = new Date(v.LocalTime);
-                    return innerY(t.getHours() + t.getMinutes() / 60);
-                })
-                .attr("r", 2)
-                .attr("fill", "red");
-
- */
-
-            cell.append('path')
-                .datum(sampledData)
-                .attr('fill', 'none')
-                .attr('stroke', '#e7d530')
-                .attr('stroke-width', 1.5)
-                .attr('d', line);
-
-        })
-
-        svg.append('g')
-            .attr('transform', `translate(0, ${height - margin.bottom})`)
-            .call(d3.axisBottom(x).ticks(width / 80));
-
-
-        svg.append('g')
-            .attr('transform', `translate(${margin.left}, 0)`)
-            .call(d3.axisLeft(y)
-                .ticks(24)
-                .tickFormat(d => `${String(d).padStart(2, '0')}:00`)
-            );
-
-        downloadSVG(ref)
-    }, [historicData, margin.bottom, margin.left, margin.right, margin.top, selectedData, rawCSV]);
 
     const closeModal = () => {
         setSelectedData(null);
@@ -419,7 +218,6 @@ const HistoricData = () => {
 
     return (
         <div className='border-2 border-gray-400 rounded-2xl h-full w-full flex flex-col relative'>
-            <svg ref={ref}></svg>
             <div id='tooltip' className='absolute bg-white text-black p-2 border border-gray-400 rounded'></div>
             {selectedData && (
                 <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center'>
@@ -429,6 +227,7 @@ const HistoricData = () => {
                     </div>
                 </div>
             )}
+            <UnitVis historicData={historicData} width={width} height={height} margin={margin}/>
             <HourOverTimeCo2/>
         </div>
     );
