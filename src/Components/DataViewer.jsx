c1e25eabedb4f175bd41d9f5cdacaeed8ae43106
JuliaHusar <juliahusar03@gmail.com>
2026-04-16 16:30:58 -0400

    added new ref for selected week

--- src/Components/DataViewer.jsx
+++ src/Components/DataViewer.jsx
@@ -1,13 +1,21 @@
 import {filterWeekData} from "./Math/HelperFunctions.js";
+import {useRef} from "react";
 
-const DataViewer = ({selectedDaypartRef, data, granularity}) => {
+const DataViewer = ({selectedDaypartRef, selectedWeekPartRef, data, granularity}) => {
     if (granularity === 24) selectedDaypartRef.current = []
     const selectedData = filterWeekData(selectedDaypartRef, data.weeklyData)
     return(
         <div>
-            {selectedData.filteredData.map((d) => (
-                <p key={d.timestamp}>{d.timestamp.toString()}</p>
-            ))}
+            {granularity === 7 ?
+                <div>
+                    {selectedData.filteredData.map((d) => (
+                        <p key={d.timestamp}>{d.timestamp.toString()}</p>
+                    ))}
+                </div>
+                :
+                <div>
+                </div>
+            }
         </div>
     )
 
